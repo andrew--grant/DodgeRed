826d473376319d9ef769408f4a6794f3e6a1e23b
andrew grant <andrewgrant@hotmail.com>
2015-05-04 20:38:40 +1000

    extended alpha duration on tween on Chris' advice

--- game/js/ScorePopover.js
+++ game/js/ScorePopover.js
@@ -23,7 +23,7 @@ ScorePopover.prototype.popInOut = function (pointsEarned, x, y) {
         self.executingPopover = true;
         var tween = game.add.tween(this)
             .to({y: this.y - 30}, 250, "Linear", false)
-            .to({alpha: 0}, 150, "Linear", false).start();
+            .to({alpha: 0}, 350, "Linear", false).start();
         tween.onComplete.add(function () {
             console.log("cb ran");
             self.executingPopover = false;
